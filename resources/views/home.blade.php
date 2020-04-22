e8dc4f8ffd59b59a1655182403e11b2b374bf4a7
takashiAg <crawd4274@gmail.com>
2020-04-22 01:51:47 +0000

    add chat function

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container-fluid">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <!-- chat list section -->
+        <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
+                <div class="panel-heading">Chat list</div>
                 <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+                    <div class="chat-item">
+                        <img class="img-circle img-responsive" src="{{asset('img/default-user.png')}}">
+                        <div class="chat-item-users">Faraz</div>
+                    </div>
                 </div>
             </div>
         </div>
+        <!-- end chat list section -->
+        <!-- message section -->
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">Chat list</div>
+                <div class="panel-body">
+                    <div>No chat</div>
+                </div>
+            </div>
+        </div>
+        <!-- end message section -->
     </div>
 </div>
 @endsection
